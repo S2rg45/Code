81e8e1abc6feaa55f36f995b433c70a34f25ec41
sergio moreno <sergio.moreno@nubiral.com>
2024-09-25 13:53:54 -0500

    Se modifica main.yaml

--- process-glue/process-analitica/process-analitica.py
+++ process-glue/process-analitica/process-analitica.py
@@ -7,20 +7,6 @@ def process_files():
     data = 0
     pass
 
-def process_files():
-    data = 0
-    pass
-
-
-def process_files():
-    data = 0
-    pass
-
-
-def process_files():
-    data = 0
-    pass
-
 
 if __name__ == __main__:
     process_files()
