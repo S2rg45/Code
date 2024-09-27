0f67ca3c474faed2e9a75fc3b683f55f9e7fcc07
sergio moreno <sergio.moreno@nubiral.com>
2024-09-27 17:27:24 -0500

    MOD:Se agrega nuevo process

--- process-glue/process-sna/process-sna.py
+++ process-glue/process-sna/process-sna.py
@@ -20,6 +20,22 @@ def process_files():
     data = 0
     pass
 
+def process_files():
+    data = 0
+    pass
+
+def process_files():
+    data = 0
+    pass
+
+
+def process_files():
+    data = 0
+    pass
+
+def process_files():
+    data = 0
+    pass
 
 if __name__ == __main__:
     process_files()
