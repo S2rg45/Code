bff1e661d68118b70ecaf5bd5ec3e505336b9b2f
sergio moreno <sergio.moreno@nubiral.com>
2024-09-30 22:19:20 -0500

    MOD: se modifica process-analitica

--- process-glue/process-analitica/process-analitica.py
+++ process-glue/process-analitica/process-analitica.py
@@ -11,14 +11,6 @@ def process_files():
     data = 0
     pass
 
-def process_files():
-    data = 0
-    pass
-
-def process_files():
-    data = 0
-    pass
-
 
 if __name__ == __main__:
     process_files()
