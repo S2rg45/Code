2867b63ab1d68fee18a54b8f25632fcfe1ebc594
sergio moreno <sergio.moreno@nubiral.com>
2024-10-01 21:18:16 -0500

    ADD: se adiciona process-analisis

---
+++ process-glue/process-analisis/process-analisis.py
@@ -0,0 +1,24 @@
+import os
+import json
+import requests
+import base64
+
+def process_files():
+    data = 0
+    pass
+
+def process_files():
+    data = 0
+    pass
+
+def process_files():
+    data = 0
+    pass
+
+def process_files():
+    data = 0
+    pass
+
+
+if __name__ == __main__:
+    process_files()
