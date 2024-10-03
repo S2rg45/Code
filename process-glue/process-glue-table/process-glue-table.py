b5e91c10a4d1677fa4784f4c11c549b101f8f316
sergio moreno <sergio.moreno@nubiral.com>
2024-10-03 16:46:52 -0500

    ADD: se adiciona proceso glue-table

---
+++ process-glue/process-glue-table/process-glue-table.py
@@ -0,0 +1,26 @@
+import os
+import json
+import requests
+import base64
+
+def process_files():
+    data = 0
+    pass
+
+def process_files():
+    data = 0
+    pass
+
+
+def process_files():
+    data = 0
+    pass
+
+
+def process_files():
+    data = 0
+    pass
+
+
+if __name__ == __main__:
+    process_files()
